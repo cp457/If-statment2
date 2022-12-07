920809f9a7c147afcb8e7b5d3bbb472b1868ec2b
cp457 <112082288+cp457@users.noreply.github.com>
2022-12-07 16:03:01 +0100

    Add files via upload

---
+++ If statment2/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String mySurname = "Priolo";
+
+        System.out.println(mySurname.contains("ni") ? "Ok let's go" : "Hmmmm i don't know");
+
+
+    }
+}
+
+/*
+    define a variable called mySurname and assign your surname to it
+    using the ternary operator, determine if your surname contains the sequence of letters ni
+    print the result in this way: Your surname [contains|doesn't contain] the sequence of letters 'ni'
+*/
